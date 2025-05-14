84f4fe8817e2206c3afe235a98266ace83ddfcdc
carrusca811 <carrusca811@gmail.com>
2025-05-14 22:58:42 +0100

    versao 15 angular

--- NetflixClone/src/app/app.module.ts
+++ NetflixClone/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
